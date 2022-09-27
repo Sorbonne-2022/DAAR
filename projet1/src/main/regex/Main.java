f614ac59dbe6540b0d1beb15e731253c675500fb
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2022-09-27 22:08:38 +0200

    first commit

---
+++ projet1/src/main/regex/Main.java
@@ -0,0 +1,63 @@
+package src.main.regex;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Main {
+    private static String regEx;
+    private static String text;
+
+    private static RegEx regExComponent = new RegEx();
+
+    public static String readFileAsString(String filePath) throws IOException {
+        StringBuilder fileData = new StringBuilder();
+        BufferedReader reader = new BufferedReader(
+                new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead = 0;
+        while ((numRead = reader.read(buf)) != -1) {
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+        }
+        reader.close();
+        return fileData.toString();
+    }
+
+    //MAIN
+    public static void main(String arg[]) throws IOException {
+        if (arg.length > 1) {
+            regEx = arg[0];
+            text = readFileAsString(arg[1]);
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("  >> Please enter a regEx: ");
+            regEx = scanner.next();
+        }
+        System.out.println("  >> Parsing regEx: \"" + regEx + "\".");
+        System.out.println("  >> Your file path: " + arg[1]);
+        System.out.println("  >> Text: \n" + text);
+        System.out.println("  >> ...");
+
+        if (regEx.length() < 1) {
+            System.err.println("  >> ERROR: empty regEx.");
+        } else {
+            System.out.print("  >> ASCII codes: [" + (int) regEx.charAt(0));
+            for (int i = 1; i < regEx.length(); i++) System.out.print("," + (int) regEx.charAt(i));
+            System.out.println("].");
+            try {
+                RegExTree ret = regExComponent.parse(regEx);
+                System.out.println("  >> Tree result: " + ret.toString() + ".");
+            } catch (Exception e) {
+                System.err.println("  >> ERROR: syntax error for regEx \"" + regEx + "\".");
+            }
+        }
+
+        System.out.println("  >> ...");
+        System.out.println("  >> Parsing completed.");
+    }
+}
---
+++ projet1/src/main/regex/NDFAutomaton.java
@@ -0,0 +1,27 @@
+package src.main.regex;
+
+import java.util.ArrayList;
+
+class NDFAutomaton {
+    //IMPLICIT REPRESENTATION HERE: INIT STATE IS ALWAYS 0; FINAL STATE IS ALWAYS transitionTable.length-1
+    protected int[][] transitionTable; //ASCII transition
+    protected ArrayList<Integer>[] epsilonTransitionTable; //epsilon transition list
+
+    public NDFAutomaton(int[][] transitionTable, ArrayList<Integer>[] epsilonTransitionTable) {
+        this.transitionTable = transitionTable;
+        this.epsilonTransitionTable = epsilonTransitionTable;
+    }
+
+    //PRINT THE AUTOMATON TRANSITION TABLE
+    public String toString() {
+        String result = "Initial state: 0\nFinal state: " + (transitionTable.length - 1) + "\nTransition list:\n";
+        for (int i = 0; i < epsilonTransitionTable.length; i++)
+            for (int state : epsilonTransitionTable[i])
+                result += "  " + i + " -- epsilon --> " + state + "\n";
+        for (int i = 0; i < transitionTable.length; i++)
+            for (int col = 0; col < 256; col++)
+                if (transitionTable[i][col] != -1)
+                    result += "  " + i + " -- " + (char) col + " --> " + transitionTable[i][col] + "\n";
+        return result;
+    }
+}
