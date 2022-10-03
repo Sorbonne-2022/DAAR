c63b6a68571e96f4b00ec08c13fe86559b77f00e
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2022-10-04 00:50:30 +0200

    KMP algo

--- projet1/src/main/regex/kmp/Main.java
+++ projet1/src/main/regex/kmp/Main.java
@@ -21,6 +21,27 @@ public class Main {
         }
         System.out.println("  >> Factor: \"" + factor + "\".");
         System.out.println("  >> Your file path: " + arg[1]);
+
+        long start = System.currentTimeMillis();
+
+        KMP kmp = new KMP(factor);
+
+        String[] lines = text.split("\n");
+
+        int lineLen = lines.length;
+
+        for (int i = 0; i < lineLen; i++) {
+            kmp.setText(lines[i]);
+            if (kmp.searchStringInText()) {
+                System.out.println("  >> Line " + (i + 1) + " : " + lines[i]);
+            }
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("  >> Time complexity " + (end - start));
+
+
         System.out.println("  >> ...");
 
         if (factor.length() < 1) {
