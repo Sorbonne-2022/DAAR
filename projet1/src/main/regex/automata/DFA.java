fa0ce56dd0c9c66005c34e778ef43e475a64411e
toancqb <toantqcqb@gmail.com>
2022-10-17 15:24:44 +0200

    refactor DFA

--- projet1/src/main/regex/automata/DFA.java
+++ projet1/src/main/regex/automata/DFA.java
@@ -1,303 +1,204 @@
 package src.main.regex.automata;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 
 public class DFA {
-    private final NDFState ndf;
-    public final HashMap<Integer, ArrayList<Integer>> transitionsMap = new HashMap<>();
+    private final NDFState ndfState;
+    public final HashMap<Integer, ArrayList<Integer>> mapper = new HashMap<>();
     public ArrayList<Integer>[][] transitionTable;
-    public ArrayList<Integer> finals = new ArrayList<>();
-    private static int cpt = 1;
+    public ArrayList<Integer> finalStates = new ArrayList<>();
+    private static int counter = 1;
 
     public DFA(NDFState ndf) {
-        this.ndf = ndf;
-        this.transitionTable = new ArrayList[1][this.ndf.getTransitionTable()[0].length];
-        for (int i = 0; i < this.transitionTable.length; i++) {
+        this.ndfState = ndf;
+        initTransitionTable();
+        toDFA();
+    }
+
+    private void initTransitionTable() {
+        this.transitionTable = new ArrayList[1][this.ndfState.getTransitionTable()[0].length];
+        for(ArrayList<Integer>[] subTab: this.transitionTable) {
             for (int j = 0; j < this.transitionTable[0].length; j++) {
-                this.transitionTable[i][j] = new ArrayList<>();
+                subTab[j] = new ArrayList<>();
             }
         }
-        determine();
-        System.out.println(" >> DFA construction:\n\nBEGIN DFA\n" + this);
-        minimize();
-        System.out.println(" >> DFA minimization:\n" + this);
-        System.out.println("END DFA.\n");
     }
 
-    private void enlargeTransitionTable() {
-        ArrayList<Integer>[][] newTransitionTable = new ArrayList[this.transitionTable.length
-                * 2][this.transitionTable[0].length];
-        for (int i = 0; i < this.transitionTable.length; i++) {
-            for (int j = 0; j < this.transitionTable[i].length; j++) {
-                newTransitionTable[i][j] = this.transitionTable[i][j];
-            }
+    private void expandingTable() {
+        int nbLines = this.transitionTable.length*2, nbCols = this.transitionTable[0].length;
+        ArrayList<Integer>[][] newTable = new ArrayList[nbLines][nbCols];
+        int index = 0;
+        for(ArrayList<Integer>[] subTab: this.transitionTable) {
+            System.arraycopy(
+                    subTab,
+                    0,
+                    newTable[index++],
+                    0,
+                    subTab.length
+            );
         }
-
-        for (int i = this.transitionTable.length; i < newTransitionTable.length; i++) {
-            for (int j = 0; j < newTransitionTable[i].length; j++) {
-                newTransitionTable[i][j] = new ArrayList<>();
+        index = this.transitionTable.length;
+        while (index < newTable.length) {
+            for (int j = 0; j < newTable[index].length; j++) {
+                newTable[index][j] = new ArrayList<>();
             }
+            index++;
         }
-        this.transitionTable = newTransitionTable;
+        this.transitionTable = newTable;
     }
 
-    private void determine() {
-        determine_step1();
-        determine_step2();
-        determine_step3();
+    private void toDFA() {
+        initProcess();
+        processingDFA();
+        finalSet();
     }
 
-    private void determine_step1() {
-        ArrayList<Integer> epsilons = new ArrayList<>();
-        epsilons.add(0);
+    private void initProcess() {
+        ArrayList<Integer> epsilons = new ArrayList<>(0);
         epsilons.addAll(getEpsilons(0));
         for (Integer eps : epsilons) {
-            for (int i = 0; i < this.ndf.getTransitionTable()[eps].length; i++) {
-                if (this.ndf.getTransitionTable()[eps][i] != -1) {
-                    this.transitionTable[0][i].add(this.ndf.getTransitionTable()[eps][i]);
-                    this.transitionTable[0][i].addAll(getEpsilons(this.ndf.getTransitionTable()[eps][i]));
-                }
-                Boolean notToPut = false;
-                for (ArrayList<Integer> value : this.transitionsMap.values()) {
-                    ArrayList<Integer> removed = new ArrayList<>();
-                    removed.addAll(value);
-                    removed.removeAll(this.transitionTable[0][i]);
-                    if (value.containsAll(this.transitionTable[0][i]) && removed.size() == 0) {
-                        notToPut = true;
-                        break;
-                    }
-                }
-
-                if (!this.transitionTable[0][i].isEmpty() && !notToPut) {
-                    this.transitionsMap.put(cpt, this.transitionTable[0][i]);
-                    cpt++;
-                }
+            int index = 0;
+            while(index < this.ndfState.getTransitionTable()[eps].length) {
+                processing(eps, index);
+                index++;
             }
         }
-        this.transitionsMap.put(0, epsilons);
+        this.mapper.put(0, epsilons);
     }
 
-    private void determine_step2() {
-        int cptWhere = 1;
-        while (cptWhere < cpt) {
+    private void processing(Integer eps, int i) {
+        if (this.ndfState.getTransitionTable()[eps][i] != -1) {
+            addElements(0, i, eps);
+        }
+        boolean flag = false;
+        for (ArrayList<Integer> value : this.mapper.values()) {
+            ArrayList<Integer> tmp = getArrayList(value, this.transitionTable[0][i]);
+            if (value.containsAll(this.transitionTable[0][i]) && tmp.size() == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!this.transitionTable[0][i].isEmpty() && !flag) {
+            this.mapper.put(counter, this.transitionTable[0][i]);
+            counter++;
+        }
+    }
+
+    private void addElements(int x, int i, Integer eps) {
+        this.transitionTable[x][i].add(this.ndfState.getTransitionTable()[eps][i]);
+        this.transitionTable[x][i].addAll(getEpsilons(this.ndfState.getTransitionTable()[eps][i]));
+    }
+
+    private void processingDFA() {
+        int tmpCounter = 1;
+        while (tmpCounter < counter) {
             ArrayList<Integer> epsilons = new ArrayList<>();
-            for (int n : this.transitionsMap.get(cptWhere)) {
-                if (!epsilons.contains(n))
-                    epsilons.add(n);
-                ArrayList<Integer> resultGetEpsilons = getEpsilons(n);
-                epsilons.removeAll(resultGetEpsilons); // This line avoid duplicates.
-                epsilons.addAll(resultGetEpsilons);
+            for (int n : this.mapper.get(tmpCounter)) {
+                processingEpsilons(epsilons, n);
             }
 
-            if (this.transitionTable.length < cptWhere + 1)
-                enlargeTransitionTable();
+            if (this.transitionTable.length < tmpCounter + 1)
+                expandingTable();
 
             for (Integer eps : epsilons) {
-                for (int i = 0; i < this.ndf.getTransitionTable()[eps].length; i++) {
-                    if (this.ndf.getTransitionTable()[eps][i] != -1) {
-                        this.transitionTable[cptWhere][i].add(this.ndf.getTransitionTable()[eps][i]);
-                        this.transitionTable[cptWhere][i].addAll(getEpsilons(this.ndf.getTransitionTable()[eps][i]));
-                    }
-
-                    Boolean notToPut = false;
-                    for (ArrayList<Integer> value : this.transitionsMap.values()) {
-                        ArrayList<Integer> removed = new ArrayList<>();
-                        removed.addAll(value);
-                        removed.removeAll(this.transitionTable[cptWhere][i]);
-                        if (value.containsAll(this.transitionTable[cptWhere][i]) && removed.size() == 0) {
-                            notToPut = true;
-                            break;
-                        }
-                    }
-
-                    if (!this.transitionTable[cptWhere][i].isEmpty()
-                            && !this.transitionsMap.values().contains(this.transitionTable[cptWhere][i]) && !notToPut) {
-                        this.transitionsMap.put(cpt, this.transitionTable[cptWhere][i]);
-                        cpt++;
-                    }
+                for (int i = 0; i < this.ndfState.getTransitionTable()[eps].length; i++) {
+                    setStateMap(tmpCounter, eps, i);
                 }
             }
-            cptWhere++;
+            tmpCounter++;
         }
 
     }
 
-    private void determine_step3() {
+    private void processingEpsilons(ArrayList<Integer> epsilons, int n) {
+        if (!epsilons.contains(n))
+            epsilons.add(n);
+        ArrayList<Integer> lstEps = getEpsilons(n);
+        epsilons.removeAll(lstEps);
+        epsilons.addAll(lstEps);
+    }
+
+    private void setStateMap(int tmpCounter, Integer eps, int i) {
+        if (this.ndfState.getTransitionTable()[eps][i] != -1) {
+            addElements(tmpCounter, i, eps);
+        }
+
+        boolean flag = false;
+        for (ArrayList<Integer> value : this.mapper.values()) {
+            ArrayList<Integer> removed = getArrayList(value, this.transitionTable[tmpCounter][i]);
+            if (value.containsAll(this.transitionTable[tmpCounter][i]) && removed.size() == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!this.transitionTable[tmpCounter][i].isEmpty()
+                && !this.mapper.containsValue(this.transitionTable[tmpCounter][i]) && !flag) {
+            this.mapper.put(counter, this.transitionTable[tmpCounter][i]);
+            counter++;
+        }
+    }
+
+    private void finalSet() {
         int i = 0;
-        for (ArrayList<Integer> transition : this.transitionsMap.values()) {
-            if (transition.contains(this.ndf.getTransitionTable().length - 1))
-                this.finals.add(i, 1);
-            else
-                this.finals.add(i, 0);
-            i++;
+        for (ArrayList<Integer> transition : this.mapper.values()) {
+            i = setFinalStates(i, transition);
         }
     }
 
-    private void minimize() {
-
-        // séparer états finaux de non finaux
-        HashMap<Integer, ArrayList<Integer>> sets = new HashMap<>();
-        ArrayList<Integer> finals = new ArrayList<>();
-        ArrayList<Integer> others = new ArrayList<>();
-        int cpt = 0;
-
-        for (Integer key : this.transitionsMap.keySet()) {
-            if (this.finals.get(key) == 1)
-                finals.add(key);
-            else
-                others.add(key);
-        }
-
-        sets.put(0, finals);
-        sets.put(1, others);
-
-        int size = (new ArrayList<>(sets.keySet())).size();
-
-        while (cpt < size) {
-            if (sets.get(cpt).size() > 1) {
-                ArrayList<ArrayList<Integer>> newSet = new ArrayList<>();
-                for (int i = 0; i < sets.get(cpt).size() - 1; i++) {
-                    ArrayList<Integer> set = new ArrayList<>();
-                    for (int j = i; j < sets.get(cpt).size(); j++) {
-                        Boolean equivalent = true;
-                        for (int k = 0; k < this.transitionTable[0].length; k++) {
-                            Integer key1 = this.getKeyFromValues(this.transitionTable[sets.get(cpt).get(i)][k]);
-                            Integer key2 = this.getKeyFromValues(this.transitionTable[sets.get(cpt).get(j)][k]);
-                            if (key1 != key2) {
-                                for (Integer key : sets.keySet()) {
-                                    if ((sets.get(key).contains(key1) && !sets.get(key).contains(key2))
-                                            || (!sets.get(key).contains(key1) && sets.get(key).contains(key2))) {
-                                        equivalent = false;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                        if (equivalent) {
-                            if (!set.contains(sets.get(cpt).get(i)))
-                                set.add(sets.get(cpt).get(i));
-                            if (!set.contains(sets.get(cpt).get(j)))
-                                set.add(sets.get(cpt).get(j));
-                        }
-                    }
-                    newSet.add(set);
-                    if (!set.contains(sets.get(cpt).get(sets.get(cpt).size() - 1))) {
-                        ArrayList<Integer> array = new ArrayList<>();
-                        array.add(sets.get(cpt).get(sets.get(cpt).size() - 1));
-                        newSet.add(array);
-                    }
-                }
-                for (int i = newSet.size() - 1; i > 0; i--) {
-                    for (int j = i - 1; j > -1; j--) {
-                        if (newSet.get(j).containsAll(newSet.get(i))) {
-                            newSet.remove(i);
-                            break;
-                        }
-                    }
-                }
-                Boolean toAdd = true;
-                for (ArrayList<Integer> set : newSet) {
-                    for (ArrayList<Integer> value : sets.values()) {
-                        if (set.equals(value))
-                            toAdd = false;
-                    }
-                    if (toAdd) {
-                        sets.put(sets.size(), set);
-                    }
-                }
-                if (toAdd)
-                    sets.remove(cpt);
-            }
-            cpt++;
-            size = (new ArrayList<>(sets.keySet())).size();
-        }
-
-        for (ArrayList<Integer> set : sets.values()) {
-            Collections.sort(set);
-            if (set.size() > 1) {
-                ArrayList<Integer> newTransitions = new ArrayList<>();
-                newTransitions.addAll(this.transitionsMap.get(set.get(0)));
-                for (int i = 1; i < set.size(); i++) {
-                    if (!this.transitionsMap.get(set.get(i)).isEmpty()) {
-                        for (Integer t : this.transitionsMap.get(set.get(i))) {
-                            if (!newTransitions.contains(t)) {
-                                newTransitions.add(t);
-                            }
-                        }
-                    }
-
-                    for (int j = 0; j < this.transitionTable.length; j++) {
-                        for (int k = 0; k < this.transitionTable[0].length; k++) {
-                            if (this.transitionTable[j][k].equals(this.transitionsMap.get(set.get(i)))
-                                    || this.transitionTable[j][k].equals(this.transitionsMap.get(set.get(0)))) {
-                                this.transitionTable[j][k] = newTransitions;
-                            }
-                        }
-                    }
-
-                    for (int j = 0; j < this.transitionTable[0].length; j++)
-                        this.transitionTable[set.get(i)][j] = new ArrayList<>();
-
-                    this.transitionsMap.remove(set.get(i));
-                    this.transitionsMap.put(set.get(0), newTransitions);
-                }
-            }
-        }
+    private int setFinalStates(int i, ArrayList<Integer> transition) {
+        if (transition.contains(this.ndfState.getTransitionTable().length - 1))
+            this.finalStates.add(i, 1);
+        else
+            this.finalStates.add(i, 0);
+        return ++i;
     }
 
-    public int getKeyFromValues(ArrayList<Integer> values) {
-        for (Integer key : this.transitionsMap.keySet()) {
-            ArrayList<Integer> removed = new ArrayList<>();
-            removed.addAll(this.transitionsMap.get(key));
-            removed.removeAll(values);
-            if (this.transitionsMap.get(key).containsAll(values) && removed.isEmpty())
-                return key;
+    public int getKey(ArrayList<Integer> values) {
+        int result = -1;
+        for (Integer key : this.mapper.keySet()) {
+            ArrayList<Integer> tmp = getArrayList(this.mapper.get(key), values);
+            if (this.mapper.get(key).containsAll(values) && tmp.isEmpty()) {
+                result = key;
+                break;
+            }
         }
-        return -1;
+        return result;
+    }
+
+    private ArrayList<Integer> getArrayList(ArrayList<Integer> mapper, ArrayList<Integer> values) {
+        ArrayList<Integer> tmp = new ArrayList<>(mapper);
+        tmp.removeAll(values);
+        return tmp;
     }
 
     private ArrayList<Integer> getEpsilons(int n) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        ArrayList<Integer> resultTmp = new ArrayList<Integer>();
-        if (this.ndf.getEpsilonTransitionTable()[n].size() != 0) {
-            for (Integer eps : this.ndf.getEpsilonTransitionTable()[n]) {
-                if (!result.contains(eps))
-                    result.add(eps);
-            }
-            for (int e : result)
-                for (Integer eps : getEpsilons(e))
-                    resultTmp.add(eps);
+        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<Integer> resultTmp = new ArrayList<>();
+        if (this.ndfState.getEpsilonTransitionTable()[n].size() != 0) {
+            addEpsilons(n, result, resultTmp);
         }
         result.addAll(resultTmp);
         return result;
     }
 
-    public ArrayList<Integer>[][] getTransitionTable() {
-        return this.transitionTable;
-    }
-
-    public ArrayList<Integer> getFinals() {
-        return this.finals;
+    private void addEpsilons(int n, ArrayList<Integer> result, ArrayList<Integer> resultTmp) {
+        for (Integer eps : this.ndfState.getEpsilonTransitionTable()[n]) {
+            if (!result.contains(eps))
+                result.add(eps);
+        }
+        for (int e : result)
+            resultTmp.addAll(getEpsilons(e));
     }
 
     public String toString() {
-        StringBuilder str = new StringBuilder("{\n  0 : " + this.transitionsMap.get(0) + " : S");
-        if (this.finals.get(0) == 1)
-            str.append(" | F");
-        for (Integer key : this.transitionsMap.keySet()) {
-            if (key == 0)
-                continue;
-            str.append("\n  ").append(key).append(" : ").append(this.transitionsMap.get(key));
-            if (this.finals.get(key) == 1)
-                str.append(" : F");
-        }
-        str.append("\n}\n\nTransition list:\n");
+        StringBuilder str = new StringBuilder("\n");
         for (int i = 0; i < this.transitionTable.length; i++) {
             for (int j = 0; j < this.transitionTable[i].length; j++) {
                 if (!this.transitionTable[i][j].isEmpty())
-                    str.append(i).append(" -- ").append((char) j).append(" --> ").append(getKeyFromValues(this.transitionTable[i][j])).append("\n");
+                    str.append(i).append(" -- ").append((char) j).append(" --> ").append(getKey(this.transitionTable[i][j])).append("\n");
             }
         }
         return str.toString();
